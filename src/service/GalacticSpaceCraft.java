00af989b5878b6795c864d2eaba9795ce617beb9
jmody1 <jimit92@gmail.com>
2024-02-08 19:10:08 +0530

    chandrayaan3-tdd-assessment/commit1: Creating service APIs for Move & Turn commands

---
+++ src/service/GalacticSpaceCraft.java
@@ -0,0 +1,140 @@
+package service;
+
+public class GalacticSpaceCraft {
+	private int x,y,z;
+	private char direction;
+	
+	public GalacticSpaceCraft(int x, int y, int z, char direction) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		this.direction = direction;
+	}
+	
+	public void moveForward() {
+        switch (direction) {
+            case 'N':
+                y++;
+                break;
+            case 'S':
+                y--;
+                break;
+            case 'E':
+                x++;
+                break;
+            case 'W':
+                x--;
+                break;
+            case 'U':
+                z++;
+                break;
+            case 'D':
+                z--;
+                break;
+        }
+    }
+	
+	public void moveBackward() {
+        switch (direction) {
+            case 'N':
+                y--;
+                break;
+            case 'S':
+                y++;
+                break;
+            case 'E':
+                x--;
+                break;
+            case 'W':
+                x++;
+                break;
+            case 'U':
+                z--;
+                break;
+            case 'D':
+                z++;
+                break;
+        }
+    }
+	
+	public void turnLeft() {
+        switch (direction) {
+            case 'N':
+                direction = 'W';
+                break;
+            case 'S':
+                direction = 'E';
+                break;
+            case 'E':
+                direction = 'N';
+                break;
+            case 'W':
+                direction = 'S';
+                break;
+        }
+    }
+
+    public void turnRight() {
+        switch (direction) {
+            case 'N':
+                direction = 'E';
+                break;
+            case 'S':
+                direction = 'W';
+                break;
+            case 'E':
+                direction = 'S';
+                break;
+            case 'W':
+                direction = 'N';
+                break;
+        }
+    }
+    
+    public void turnUp() {
+        if (direction != 'U') {
+            direction = 'U';
+        }
+    }
+
+    public void turnDown() {
+        if (direction != 'D') {
+            direction = 'D';
+        }
+    }
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getZ() {
+		return z;
+	}
+
+	public void setZ(int z) {
+		this.z = z;
+	}
+
+	public char getDirection() {
+		return direction;
+	}
+
+	public void setDirection(char direction) {
+		this.direction = direction;
+	}
+    
+
+}
